4483bd275b96c7d4233e4986160f1c06594339a5
Daniel Siwczak <dsiwczak@o2.pl>
2017-03-21 13:10:59 +0100

    path

--- JS/primeNr.js
+++ JS/primeNr.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 function pierwsza(nr) {
     for(var i = 2; i < nr; i+=2) {
         if(nr % i === 0) {
@@ -6,4 +7,14 @@ function pierwsza(nr) {
     }
     return nr > 1;
 }
+=======
+function pierwsza(nr) {
+    for(var i = 2; i < nr; i+=2) {
+        if(nr % i === 0) {
+            return false;
+        }
+    }
+    return nr > 1;
+}
+>>>>>>> bc5edb90a978536c1f5739a23b26fe6e2802ec0e
 console.log(pierwsza(7));
